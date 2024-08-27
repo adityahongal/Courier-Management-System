1e42450ec77e68e1630538911d1efe738f026773
adityahongal <adityahongalwork@gmail.com>
2024-08-27 19:20:42 +0530

    Courier Management System Project

---
+++ rake.php
@@ -0,0 +1,19 @@
+<div itemprop="articleBody">
+		<h1 style="text-align: center;">ONLINE COURIER MANAGEMENT SYATEM</h1>
+<p><img style="float: left; border: 2px solid black; margin-left: 40px; margin-right: 40px;" src="/images/cse/1r.jpg" border="0" /></p>
+<div style="text-align:center">
+<h2>Dr. Rakesh L</h2>
+<p>Designation: Professor &amp; HOD<br /> Qualifications:B.E, M.Tech, Ph. D.<br /> Email: <span id="cloakee5299ee87606207c3f4212fde414d42">This email address is being protected from spambots. You need JavaScript enabled to view it.</span><script type='text/javascript'>
+				document.getElementById('cloakee5299ee87606207c3f4212fde414d42').innerHTML = '';
+				var prefix = '&#109;a' + 'i&#108;' + '&#116;o';
+				var path = 'hr' + 'ef' + '=';
+				var addyee5299ee87606207c3f4212fde414d42 = 'dr.r&#97;k&#101;sh168' + '&#64;';
+				addyee5299ee87606207c3f4212fde414d42 = addyee5299ee87606207c3f4212fde414d42 + 'gm&#97;&#105;l' + '&#46;' + 'c&#111;m';
+				var addy_textee5299ee87606207c3f4212fde414d42 = 'dr.r&#97;k&#101;sh168' + '&#64;' + 'gm&#97;&#105;l' + '&#46;' + 'c&#111;m';document.getElementById('cloakee5299ee87606207c3f4212fde414d42').innerHTML += '<a ' + path + '\'' + prefix + ':' + addyee5299ee87606207c3f4212fde414d42 + '\'>'+addy_textee5299ee87606207c3f4212fde414d42+'<\/a>';
+		</script> <br /> 
+<p><a class="link-button button-blue"><span class="link-icon jsn-icon-User"></span></a></p>
+<hr />
+<p><img style="float: left; border: 2px solid black; margin-left: 40px; margin-right: 40px;text-align:center; src="images/rakesh.JPG" border="0" /></p>
+    </div>
+	</div>
+	
